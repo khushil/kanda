6df7637e762b66192b85ecbf46fa30d294a86c6f
Khushil Dep <khushil.dep@gmail.com>
2020-07-03 23:01:10 +0100

    Setup prefabs

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    #region Public Variables
+    
+    [SerializeField] GameObject cameraTarget;
+    
+    #endregion
+    
+    #region Private Variables
+    
+    private Vector3 offsetToPlayer;
+    
+    #endregion
+    // Start is called before the first frame update
+    void Start()
+    {
+        offsetToPlayer = transform.position - cameraTarget.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = cameraTarget.transform.position + offsetToPlayer;
+    }
+}
---
+++ Assets/Scripts/FuelDepotController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FuelDepotController : MonoBehaviour
+{
+   
+    #region Private Variables
+    
+    private float _fuel = 10f;
+    private float _refuelRate = 0.1f;
+    
+    #endregion
+    
+    #region Public Variables
+    
+
+    
+    #endregion
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+
+    }
+}
