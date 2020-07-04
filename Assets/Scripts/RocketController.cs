c3b6e7901541b9a154a4bdad5ddb894039d89f27
Khushil Dep <khushil.dep@gmail.com>
2020-07-04 09:19:43 +0100

    Add a nose cone to the rocket prefab and the rocket controller

--- Assets/Scripts/RocketController.cs
+++ Assets/Scripts/RocketController.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class RocketController : MonoBehaviour
 {
+    private Rigidbody rigidbody;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rigidbody.AddRelativeForce(Vector3.up);
     }
 }
